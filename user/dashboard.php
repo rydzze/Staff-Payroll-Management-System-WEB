5c9f3667075a372aaa7c95a3b15bd51760282665
Blackthorn23 <153483743+Blackthorn23@users.noreply.github.com>
2024-06-29 16:17:00 +0800

    Update dashboard.php

--- user/dashboard.php
+++ user/dashboard.php
@@ -7,16 +7,50 @@
     <title>SPMS | Dashboard</title>
 
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/dashboard.css">
 </head>
 
 <body>
     <?php include 'templates/sidebar.php';?>
+<main>
+        <div class="container">
+            <div class="sidebar">
+                <div class="sidebar-item">
+                    <img src="staf.png">
+                    <i class="staff"></i> Staff
+                </div>
+                <div class="sidebar-item">
+                    <img src="About.png">
+                    <i class="about"></i> About
+                </div>
+                <div class="sidebar-item">
+                    <img src="Payroll.png">
+                    <i class="payroll"></i> Payroll
+                </div>
+            </div>
+            <div class="main">
+                <div class="bulletin-board">
+                    <div class="title">
+                        <i class="fas fa-chevron-left"></i> Bulletin Board <i class="fas fa-chevron-right"></i>
+                    </div>
+                    <div class="content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, voluptas!
+                        </p>
+                    </div>
+                </div>
+                <div class="announcement">
+                    <h3>Announcement</h3>
+                    <p>
+                        Bla bla bla
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
 
-    <section class="content">
-        <h1>Welcome back!</h1>
-        
-        <h2>Dashboard</h2>
-    </section>
+ 
 </body>
 
 </html>
