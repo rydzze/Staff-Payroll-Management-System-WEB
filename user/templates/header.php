a675f16490ef8523f4a03b7a838d44a4c8123c73
Iffat Hanafiah <muhammadiffath@gmail.com>
2024-06-29 18:08:04 +0800

    edit header.php for user

--- user/templates/header.php
+++ user/templates/header.php
@@ -1,13 +1,17 @@
+<html>
+<link rel="stylesheet" href="../css/header.css">
 <div class="header">
-    <h1>Staff Payroll System</h1>
-
+    <div class="heading">
+        <h1>Staff Payroll System</h1>
+    </div>
     <div class="profiledropdown">
-        <img src="" alt="profilepicture">
-        <p>Muhammad Ali</p>
-        <img src="" alt="dropdown">
+        <img src="../img/user.jpg" class="profile" alt="profilepicture">
+        <span>Muhammad Ali</span>
+        <img src="../img/dropdown.png" class="dropdown" alt="dropdown">
         <div class="dropdown-content">
             <a href="viewuser.php">View Profile</a>
             <a href="includes/session_end.php">Log Out</a>
         </div>
     </div>
 </div>
+</html>
