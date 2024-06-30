43afda436c024ee00fc75b11ffced39c4e8721c1
Blackthorn23 <153483743+Blackthorn23@users.noreply.github.com>
2024-06-30 19:11:07 +0800

    viewuser

--- user/viewuser.php
+++ user/viewuser.php
@@ -21,6 +21,75 @@
         <p>Staff ID : 1221301131 </p>    
     </div>
     </section>
+    <section class="info">
+                <div class="card0">
+                  <div class="card-title"><br>Residential Address</div>
+                  <div class="card-content">
+                    <h3>No. 12, Jalan Bukit Bintang, 55100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur, Malaysia.</h3>
+                  </div>
+                  </div>
+                </div>
+
+    <table>
+        <tr>
+            <td>
+                <div class="card1">
+                  <div class="card-title"><br>Contact Details</div>
+                  <div class="card-content">
+                    <h3>+6010-8096754</h3>
+                  </div>
+                  </div>
+                </div>
+            </td>
+            <td>
+                <div class="card2">
+                  <div class="card-title"><br>Department</div>
+                  <div class="card-content">
+                    <h3>Engineering</h3>
+                  </div>
+                  </div>
+                </div>
+            </td>
+            <td>
+                <div class="card3">
+                  <div class="card-title"><br>Position</div>
+                  <div class="card-content">
+                    <h3>Chief Officer</h3>
+                  </div>
+                  </div>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div class="card1">
+                  <div class="card-title"><br>Employement Status</div>
+                  <div class="card-content">
+                    <h3>Full-Time</h3>
+                  </div>
+                </div>
+            </td>
+            <td>
+                <div class="card2">
+                  <div class="card-title"><br>Performance</div>
+                  <div class="card-content">
+                    <h3>Excellence Workers</h3>
+                  </div>
+                  </div>
+                </div>
+            </td>
+            <td>
+                <div class="card3">
+                  <div class="card-title"><br>Date of Hire</div>
+                  <div class="card-content">
+                    <h3>19/05/1999</h3>
+                  </div>
+                  </div>
+                </div>
+            </td>
+        </tr>
+    </table>
+    </section>
 </body>
 
 </html>
