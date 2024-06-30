2a1e0f64e645d4425069cda4a5fa9418dfae729f
Blackthorn23 <153483743+Blackthorn23@users.noreply.github.com>
2024-06-30 18:11:39 +0800

    sidebar and viewuser

--- user/templates/sidebar.php
+++ user/templates/sidebar.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="css/sidebar.css">
 <div class="sidebar">
     <ul>
-        <li><img src="img/staff.png"><a href="dashboard.php">Staff</a></li>
+        <li><img src="img/home.png"><a href="dashboard.php">Home</a></li>
         <li><img src="img/about.png"><a href="about.php">About</a></li>
         <li><img src="img/payroll.png"><a href="viewpayroll.php">Payroll</a></li>
     </ul>
--- user/viewuser.php
+++ user/viewuser.php
@@ -4,17 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SPMS | View Payroll</title>
+    <title>SPMS | View User</title>
 
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/viewuser.css">
 </head>
 
 <body>
+    <?php include 'templates/header.php';?>
     <?php include 'templates/sidebar.php';?>
 
     <section class="content"> 
-        <h1>viewuser</h1>
-        
+    <div class="profile">
+        <img src="img/profile.png"> 
+        <h1>Muhammad Ali</h1>   
+        <p>Staff ID : 1221301131 </p>    
+    </div>
     </section>
 </body>
 
