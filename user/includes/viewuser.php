3de7ba44f8dcacffb41e7c452cd0885f0cb3b7f1
Blackthorn23 <153483743+Blackthorn23@users.noreply.github.com>
2024-07-01 18:19:40 +0800

    viewuser.css and viewuser.php

---
+++ user/includes/viewuser.php
@@ -0,0 +1,27 @@
+<?php
+include 'db.php';
+
+$staff_ID = $_SESSION['staff_ID'];
+
+if (!isset($staff_ID)) {
+    echo "<script>alert('Staff ID not provided.'); window.location.href = 'staffs.php';</script>";
+    exit;
+}
+
+$sql = "SELECT  p.person_phonenum, p.person_homeaddr, p.person_email, s.staff_department, s.staff_position, s.staff_basicsalary, s.staff_hireddate, s.staff_status
+        FROM person p
+        INNER JOIN staff s ON p.staff_ID = s.staff_ID
+        WHERE p.staff_ID = '$staff_ID'"; 
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+
+$residential =  $row["person_homeaddr"];
+$contact = $row["person_phonenum"];
+$department = $row["staff_department"];
+$position = $row["staff_position"];
+$email = $row["person_email"];
+$salary = $row["staff_basicsalary"];
+$hireddate = $row["staff_hireddate"];
+
+?>
+    
--- user/viewuser.php
+++ user/viewuser.php
@@ -1,4 +1,8 @@
-<?php include 'includes/session_start.php'; ?>
+<?php include 'includes/session_start.php';
+include 'includes/viewuser.php';
+ $name = $_SESSION['person_name'];
+ $staff_ID = $_SESSION['staff_ID'];
+ ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,8 +23,8 @@
     <section class="content"> 
       <div class="profile">
           <img src="img/profile.png"> 
-          <h1>Muhammad Ali</h1>   
-          <p>Staff ID : 1221301131 </p>    
+          <h1><?php echo $name?></h1>
+          <p>Staff ID: <?php echo $staff_ID?></p>
       </div>
     </section>
 
@@ -28,7 +32,7 @@
       <div class="card0">
         <div class="card-title"><br>Residential Address</div>
         <div class="card-content">
-          <h3>No. 12, Jalan Bukit Bintang, 55100 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur, Malaysia.</h3>
+          <h3><?php echo $residential?></h3>
         </div>
         </div>
       </div>
@@ -39,7 +43,7 @@
                 <div class="card1">
                   <div class="card-title"><br>Contact Details</div>
                   <div class="card-content">
-                    <h3>+6010-8096754</h3>
+                    <h3><?php echo $contact?></h3>
                   </div>
                   </div>
                 </div>
@@ -48,7 +52,7 @@
                 <div class="card2">
                   <div class="card-title"><br>Department</div>
                   <div class="card-content">
-                    <h3>Engineering</h3>
+                    <h3><?php echo $department?></h3>
                   </div>
                   </div>
                 </div>
@@ -57,7 +61,7 @@
                 <div class="card3">
                   <div class="card-title"><br>Position</div>
                   <div class="card-content">
-                    <h3>Chief Officer</h3>
+                    <h3><?php echo $position?></h3>
                   </div>
                   </div>
                 </div>
@@ -66,17 +70,17 @@
         <tr>
             <td>
                 <div class="card1">
-                  <div class="card-title"><br>Employement Status</div>
+                  <div class="card-title"><br>Email</div>
                   <div class="card-content">
-                    <h3>Full-Time</h3>
+                    <h3><?php echo $email?></h3>
                   </div>
                 </div>
             </td>
             <td>
                 <div class="card2">
-                  <div class="card-title"><br>Performance</div>
+                  <div class="card-title"><br>Salary</div>
                   <div class="card-content">
-                    <h3>Excellence Workers</h3>
+                    <h3><?php echo "RM ". $salary?></h3>
                   </div>
                   </div>
                 </div>
@@ -85,7 +89,7 @@
                 <div class="card3">
                   <div class="card-title"><br>Date of Hire</div>
                   <div class="card-content">
-                    <h3>19/05/1999</h3>
+                    <h3><?php echo $hireddate?></h3>
                   </div>
                   </div>
                 </div>
